d8562bce6587cdea7d2644105d115da4d272c3bb
Rodrigo <r.aug@hotmail.com>
2025-01-09 20:49:07 -0300

    App working with dummy data

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -27,4 +30,31 @@ private fun BookSearchBarPreview() {
             .fillMaxWidth()
     )
 
+}
+
+private val books = (1..100).map {
+    Book(
+       id = it.toString(),
+        title = "Book $it",
+        imageUrl = "test",
+        authors = listOf("Rodrigo Augusto"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
